e846b9d8971c02fa7a10383401bdc54ef115f67b
Yogesh Pandey <ypandey9@gmail.com>
2025-11-09 10:19:52 +0530

    Card with Header and Footer Props (advanced pattern)

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
-import { useState } from "react";
+import Card from "./components/Card";
+
 
 export default function App() {
-
-  const [count,setCount]=useState(0);
-
   return (
-    <div>
-      <p>You clicked {count} times</p>
-      <button onClick={()=>setCount(count+1)}>➕</button>
-      <button onClick={()=>setCount(count-1)}>➖</button>
-      <button onClick={()=>setCount(0)}>🔃</button>
-    </div>
+ <Card
+ header={<h3 style={{color:"blue"}}>React Composition</h3>}
+ footer={<small>© 2025 React Academy</small>}
+ >
+<p>This is the main content of the card.</p>
+<button>Learn more</button>
+ </Card>
+ 
   );
-
 }
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,13 +1,18 @@
-function Card({title,children}) {
-
-    return (
-
-        <div style={{border:"1px solid gray",padding:"10px"}}>
-            <h3>{title}</h3>
-            <div>{children}</div>
-        </div>
-    );
-
+function Card({ header, footer, children }) {
+  return (
+    <div style={{
+      border: "1px solid gray",
+      padding: "10px",
+      borderRadius: "8px",
+      marginBottom: "15px",
+      marginLeft:"30px",
+    }}>
+      <div>{header}</div>
+      <div>{children}</div>
+      <div style={{ marginTop: "10px" }}>{footer}</div>
+    </div>
+  );
 }
+
 
 export default Card;
